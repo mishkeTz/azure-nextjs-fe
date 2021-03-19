41c8feb8f4a72bdc3abb6b3d03a9664d1547bd61
Nikola Misic <nikola.misic@bitgrip.de>
2021-03-19 10:39:27 +0100

    Initial cm

---
+++ auth.sh
@@ -0,0 +1,5 @@
+# bash
+export azureSubId='dc479e99-13bb-4ab5-93e9-125022729006'
+export azureServicePrincipalTenantId='2a53fbf1-fcee-4d11-9d5a-9cc50fe3e63e'
+export azureServicePrincipalClientId='test1'
+export azureServicePrincipalPassword='test1'
